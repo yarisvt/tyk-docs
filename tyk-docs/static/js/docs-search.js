ae969faf64446e6d868c203300a113614ef8e7e7
Konstantinos Markopoulos <kos@tyk.io>
2017-05-03 17:59:54 +0100

    Removed searchly configs, modified config to generate json files

--- tyk-docs/static/js/docs-search.js
+++ tyk-docs/static/js/docs-search.js
@@ -1,9 +1,9 @@
-  // docsearch({
-  // apiKey: '1f3b99e94e942b5bc7b26a3a0edf49cd',
-  // indexName: 'tyk',
-  // inputSelector: '.searchBox',
-  // debug: true ,
-  // algoliaOptions: {
-  //   hitsPerPage: 20
-  // }
-  // });
+  docsearch({
+  apiKey: '1f3b99e94e942b5bc7b26a3a0edf49cd',
+  indexName: 'tyk',
+  inputSelector: '.searchBox',
+  debug: true ,
+  algoliaOptions: {
+    hitsPerPage: 20
+  }
+  });
