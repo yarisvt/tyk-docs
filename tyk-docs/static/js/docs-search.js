419bc4ccee7e7a36cf417dd7eaf11abda4c55bee
Konstantinos Markopoulos <kos@tyk.io>
2017-05-05 16:31:54 +0100

    Improved search functionality, added testing styling

--- tyk-docs/static/js/docs-search.js
+++ tyk-docs/static/js/docs-search.js
@@ -1,24 +1,27 @@
 var search = instantsearch({
   appId: 'EIXQM46UN9',
-  apiKey: '2ac5e9a2564f4a3a187ab44500f42062',
+  apiKey: '10c29e867eddcb37b72bf886a3bb1acf',
   indexName: 'tyk-test',
-  urlSync: {},
+  query: 'query',
+  advancedSyntax: true,
   searchFunction(helper) {
-  	var hits = document.getElementById("hits");
-  	var pagination = document.getElementById("pagination");
-
-    setTimeout(history.replaceState(null, "", "?q="), 750);
+    var hits = document.getElementById("hits");
+    var pagination = document.getElementById("pagination");
 
     if (helper.state.query === '') {
-    	hits.style.display = 'none';
-    	pagination.style.display = 'none';
+        hits.style.display = 'none';
+        pagination.style.display = 'none';
       return;
     }
 
-		hits.style.display = 'block';
-		pagination.style.display = 'block';
+    helper.setQueryParameter('attributesToSnippet', ['article:10']);
+    helper.setQueryParameter('advancedSyntax', true).search();
+
+    hits.style.display = 'block';
+    pagination.style.display = 'block';
 
     helper.search();
+
   }
 });
 
@@ -26,14 +29,14 @@ search.addWidget(
   instantsearch.widgets.searchBox({
     container: '#q'
   })
-);
+); 
 
 
 var hitTemplate =
   '<div class="hit media">' +
     '<div class="media-body">' +
-      '<h4 class="media-heading">{{{_highlightResult.title.value}}}</h4>' +
-      '<p class="year">{{year}}</p><p><span class="badge">{{.}}</span></p>' +
+      '<h4 class="media-heading">Title: {{{_highlightResult.title.value}}}</h4></p> </a>' +
+      '<h4 class="media-heading em">Content: {{{_snippetResult.article.value}}}</h4></p> </a>' +
     '</div>' +
   '</div>';
 
@@ -43,10 +46,10 @@ var noResultsTemplate =
 search.addWidget(
   instantsearch.widgets.hits({
     container: '#hits',
-    hitsPerPage: 10,
+    hitsPerPage: 5,
     templates: {
       empty: noResultsTemplate,
-      item: hitTemplate
+      item: hitTemplate,
     }
   })
 );
@@ -60,5 +63,6 @@ search.addWidget(
     }
   })
 );
+
 
 search.start();
