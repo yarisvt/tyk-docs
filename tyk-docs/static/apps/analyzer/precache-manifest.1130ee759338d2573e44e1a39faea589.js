768c638afce9ed12199594ed2268b38fd935211d
Zaid Albirawi <zaid@tyk.io>
2022-09-07 10:35:36 -0400

    Modify analyzer app (#1991)
    * Update apps and fix URLs
    
    * Add html pages
    
    (cherry picked from commit 7c5f0018b1d062393e15ea1b14da486bf2735f24)

---
+++ tyk-docs/static/apps/analyzer/precache-manifest.1130ee759338d2573e44e1a39faea589.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "192c422c0223774ce790",
+    "url": "/docs/apps/analyzer/static/css/main.0f98707c.chunk.css"
+  },
+  {
+    "revision": "192c422c0223774ce790",
+    "url": "/docs/apps/analyzer/static/js/main.192c422c.chunk.js"
+  },
+  {
+    "revision": "f25528436ada661202f5",
+    "url": "/docs/apps/analyzer/static/js/1.f2552843.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/docs/apps/analyzer/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "223e318531b00c51b25bc5fda0b5e959",
+    "url": "/docs/apps/analyzer/static/media/Smoolthan Bold.223e3185.otf"
+  },
+  {
+    "revision": "05046cd0a6b57dcc241b01249af452c7",
+    "url": "/index.html"
+  }
+];
