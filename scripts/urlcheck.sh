58e5ffc084d5a73f1a30335258ee74328f6ef429
Leonid Bugaev <leonsbox@gmail.com>
2022-12-11 18:31:24 +0300

    Fix url (#2120)
    Fix URL check job

--- scripts/urlcheck.sh
+++ scripts/urlcheck.sh
@@ -9,9 +9,12 @@ hugo
 cp ./public/urlcheck.json /tmp/urlcheck.prev.json
 rm -rf public/*
 git checkout -
-NEW_URLS=$(cat /tmp/urlcheck.new.json | jq -r '.path')
-PREV_URLS=$(cat /tmp/urlcheck.prev.json | jq -r '.path')
-BROKEN_URLS=$(comm -3 -1 <(echo $NEW_URLS | sort) <(echo $PREV_URLS | sort))
+
+export LC_COLLATE=POSIX
+
+cat /tmp/urlcheck.new.json | jq -r '.path' | sed -E "s|/nightly||g" | sort | uniq > /tmp/urlcheck.new
+cat /tmp/urlcheck.prev.json | jq -r '.path' | sed -E "s|/nightly||g" | sort | uniq > /tmp/urlcheck.prev
+BROKEN_URLS=$(comm -3 -1 /tmp/urlcheck.new /tmp/urlcheck.prev)
 
 if [ -n "$BROKEN_URLS" ]; then
   echo "The following links are broken"
