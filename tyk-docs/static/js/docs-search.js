500a8dba8c4b06ddaae1cab364e3ab3bd4b328b0
Vlad Ifrim <ifrimvlad@gmail.com>
2023-05-09 19:14:39 +0300

    small improvements (#2512)
    * moves the version selector under the navigation and sets the focus on the search input when pressing '/'
    
    * 'anchors' the version selector to the bottom
    
    * changes the placeholder of the search input
    
    * adds a red border to categories with no children in the sidebar navigation

--- tyk-docs/static/js/docs-search.js
+++ tyk-docs/static/js/docs-search.js
@@ -1,20 +1,32 @@
-/**
- * INIT INSTAN SEARCH
- */
+(function() {
+  let inputSelector = '#searchbox input';
 
- docsearch({
-  algoliaOptions: {
-   hitsPerPage: 200,
-   // See https://www.algolia.com/doc/api-reference/api-parameters/
-  },
-  // Your apiKey and indexName will be given to you once
-  // we create your config
-  apiKey: 'ALGOLIA_API_KEY',
-  indexName: 'docsearch',
-  appId: 'ALGOLIA_APP_ID', // Should be only included if you are running DocSearch on your own.
-  // Replace inputSelector with a CSS selector
-  // matching your search input
-  inputSelector: '#searchbox input',
-  // Set debug to true to inspect the dropdown
-  // debug: true,
-});
+  /**
+   * INIT INSTANT SEARCH
+   */
+  docsearch({
+    algoliaOptions: {
+    hitsPerPage: 200,
+    // See https://www.algolia.com/doc/api-reference/api-parameters/
+    },
+    // Your apiKey and indexName will be given to you once
+    // we create your config
+    apiKey: 'ALGOLIA_API_KEY',
+    indexName: 'docsearch',
+    appId: 'ALGOLIA_APP_ID', // Should be only included if you are running DocSearch on your own.
+    // Replace inputSelector with a CSS selector
+    // matching your search input
+    inputSelector,
+    // Set debug to true to inspect the dropdown
+    // debug: true,
+  });
+
+  document.body.addEventListener('keydown', e => {
+    let input = document.querySelector(inputSelector);
+
+    if (e.key !== '/' || !input || e.target !== document.body) return;
+
+    e.preventDefault();
+    input.focus();
+  });
+}());
