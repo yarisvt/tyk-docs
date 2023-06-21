df296d6b46bb46d6e6c73254bc0690852013216a
Simon Pears <simon@tyk.io>
2023-06-21 11:33:04 +0100

    set APIM Best Practice in tabURLS to be empty string

--- scripts/menu_generator.py
+++ scripts/menu_generator.py
@@ -191,7 +191,7 @@ with open(categories_path, "r") as file:
                     "Managing APIs": "/getting-started",
                     "Product Stack": "/tyk-stack",
                     "Developer Support": "/frequently-asked-questions/faq",
-                    "APIM Best Practice": "/getting-started/key-concepts",
+                    "APIM Best Practice": "",
                     "Orphan": "/orphan",
                 }
 
