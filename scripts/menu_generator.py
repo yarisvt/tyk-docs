8d8c37571b05f2aef70a436208e7af6fd100cbdc
dcs3spp <dcs3spp@users.noreply.github.com>
2023-06-30 15:29:07 +0100

    [DX-402] Update APIM Best Practices tab so maps to path "" : This will display home tab when clicked (#2748)
    set APIM Best Practice in tabURLS to be empty string
    
    Co-authored-by: Simon Pears <simon@tyk.io>

--- scripts/menu_generator.py
+++ scripts/menu_generator.py
@@ -192,7 +192,7 @@ with open(categories_path, "r") as file:
                     "Managing APIs": "/getting-started",
                     "Product Stack": "/tyk-stack",
                     "Developer Support": "/frequently-asked-questions/faq",
-                    "APIM Best Practice": "/getting-started/key-concepts",
+                    "APIM Best Practice": "",
                     "Orphan": "/orphan",
                 }
 
