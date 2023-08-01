73fee6f14709a7da8572a5cccf3ee05ff2bc2fb6
dcs3spp <dcs3spp@users.noreply.github.com>
2023-08-01 16:50:44 +0100

    Update urlcheck.sh to strip version number (#3037)
    Update urlcheck.sh to strip version number

--- scripts/urlcheck.sh
+++ scripts/urlcheck.sh
@@ -15,8 +15,8 @@ git checkout -
 
 export LC_COLLATE=POSIX
 
-cat /tmp/urlcheck.new.json | jq -r '.path' | sed -E "s|/nightly||g" | sed -E "s|/docs/docs||" | sed -E "s|/docs||" | sed -E "s|^/||" | sed -E "s|/$||" | sort | uniq > /tmp/urlcheck.new
-cat /tmp/urlcheck.prev.json | jq -r '.path' | sed -E "s|/nightly||g" | sed -E "s|/docs/docs||" | sed -E "s|/docs||" | sed -E "s|^/||" | sed -E "s|/$||" | sort | uniq > /tmp/urlcheck.prev
+cat /tmp/urlcheck.new.json | jq -r '.path' | sed -E "s|/nightly||g" | sed -E 's|/[0-9]+\.[0-9]+||g' | sed -E "s|/docs/docs||" | sed -E "s|/docs||" | sed -E "s|^/||" | sed -E "s|/$||" | sort | uniq > /tmp/urlcheck.new
+cat /tmp/urlcheck.prev.json | jq -r '.path' | sed -E "s|/nightly||g" | sed -E 's|/[0-9]+\.[0-9]+||g' | sed -E "s|/docs/docs||" | sed -E "s|/docs||" | sed -E "s|^/||" | sed -E "s|/$||" | sort | uniq > /tmp/urlcheck.prev
 BROKEN_URLS=$(comm -3 -1 /tmp/urlcheck.new /tmp/urlcheck.prev)
 
 if [ -n "$BROKEN_URLS" ]; then
