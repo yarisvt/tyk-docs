3672ac67aa5cd4bf386848ee39608c422c9e1f02
Leonid Bugaev <leonsbox@gmail.com>
2023-09-27 18:26:32 +0300

    Merging to release-5.1: [DX-677]change to nearest when scrolling (#3236)
    [DX-677]change to nearest when scrolling (#3236)
    
    
    * change to center

--- tyk-docs/static/js/scroll_to_active_tab.js
+++ tyk-docs/static/js/scroll_to_active_tab.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const activeItem = document.querySelector('.st-treed li.active');
     if (activeItem) {
         activeItem.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
+            behavior: 'instant',
+            block: 'center',
         });
     }
 });
